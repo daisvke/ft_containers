bf6f103c6e88e24b7547c27b715013781f89613a
daisvke BB One <dtanigaw@student.42.fr>
2022-10-19 16:21:24 +0200

    second

--- vector.hpp
+++ vector.hpp
@@ -18,3 +18,57 @@ namespace ft {
 		typedef typename allocator_type::const_pointer		const_pointer;
 		typedef std::reverse_iterator<iterator>				reverse_iterator;
 		typedef std::reverse_iterator<const_iterator>		const_reverse_iterator;
+
+
+		// 23.2.4.1 construct/copy/destroy:
+		explicit vector(const allocator_type& alloc = allocator_type()) :  {}
+		explicit vector(size_type n, const value_type& value = value_type(),
+		const Allocator& = allocator_type());
+		template <class InputIterator>
+		vector(InputIterator first, InputIterator last,
+		const Allocator& = allocator_type());
+		vector(const vector<T,Allocator>& x);
+		~vector();
+		vector<T,Allocator>& operator=(const vector<T,Allocator>& x);
+		template <class InputIterator>
+		void assign(InputIterator first, InputIterator last);
+		void assign(size_type n, const T& u);
+		allocator_type get_allocator() const;
+		// iterators:
+		iterator begin();
+		const_iterator begin() const;
+		iterator end();
+		const_iterator end() const;
+		reverse_iterator rbegin();
+		const_reverse_iterator rbegin() const;
+		reverse_iterator rend();
+		const_reverse_iterator rend() const;
+		// 23.2.4.2 capacity:
+		size_type size() const;
+		size_type max_size() const;
+		void resize(size_type sz, T c = T());
+		size_type capacity() const;
+		bool empty() const;
+		void reserve(size_type n);
+		// element access:
+		reference operator[](size_type n);
+		const_reference operator[](size_type n) const;
+		const_reference at(size_type n) const;
+		reference at(size_type n);
+		reference front();
+		const_reference front() const;
+		reference back();
+		const_reference back() const;
+		// 23.2.4.3 modifiers:
+		void push_back(const T& x);
+		void pop_back();
+		iterator insert(iterator position, const T& x);
+		void insert(iterator position, size_type n, const T& x);
+		template <class InputIterator>
+		void insert(iterator position,
+		InputIterator first, InputIterator last);
+		iterator erase(iterator position);
+		iterator erase(iterator first, iterator last);
+		void swap(vector<T,Allocator>&);
+		void clear();
+		};
