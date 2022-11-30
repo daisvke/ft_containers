05392ecf53a38f1bdee2f08fc5a7aa6259dea533
dtanigaw <dtanigaw@student.42.fr>
2022-11-30 03:26:22 +0100

    Add make + make_pair

--- Makefile
+++ Makefile
@@ -1,17 +1,39 @@
+# E X E C U T A B L E  N A M E #
+
 NAME			=	ft_containers
+
+
+# C X X  F L A G S #
+
 CXX				=	c++
 CXXFLAGS		=	-Wall -Wextra -std=c++98 #-Werror
 DEBUG			=	-g3
 
+
+# S O U R C E  F I L E S #
+
 SRCS			=	main.cpp
+
+
+# O B J .  F I L E S #
+
 OBJS_DIR		=	objs/
 OBJS_FILES		=	$(SRCS:.cpp=.o)
 OBJS			=	$(addprefix $(OBJS_DIR), $(OBJS_FILES))
-INCS			=	algorithm.hpp \
+
+
+# I N C .  F I L E S #
+
+INC_DIR			=	incs/
+INC_FILES		=	algorithm.hpp \
 					vector.hpp \
 					type_traits.hpp \
 					iterator.hpp \
 					utility.hpp
+INCS			=	$(addprefix $(INC_DIR), $(INC_FILES))
+
+
+#  B U I L D  R U L E S #
 
 .PHONY: all clean fclean re
 
@@ -20,9 +42,12 @@ all: $(NAME)
 $(NAME): $(OBJS)
 		$(CXX) -o $@ $(OBJS)
 
-$(OBJS): $(SRCS)
+$(OBJS): $(SRCS) $(INCS)
 		mkdir -p objs/
-		$(CXX) $(CXXFLAGS) $(DEBUG) -c $< -o $@
+		$(CXX) $(CXXFLAGS) -I$(INC_DIR) $(DEBUG) -c $< -o $@
+
+
+# C L E A N  &  O T H E R  R U L E S #
 
 clean:
 	rm -rf $(OBJS_DIR)
---
+++ incs/utility.hpp
@@ -0,0 +1,71 @@
+#ifndef UTILITY_HPP
+# define UTILITY_HPP
+
+namespace ft
+{
+	/*************************************************************
+	* PAIR 
+	*************************************************************/
+	template<class T1, class T2>
+	struct pair
+	{
+		typedef T1	first_type;
+		typedef T2	second_type;
+
+		first_type	first;
+		second_type	second;
+
+		pair() : first(), second() {}
+		pair(const first_type &a, second_type &b) : first(a), second(b) {}
+
+		template<class U1, class U2>
+		pair(const pair<U1, U2> &p) : first(p.first, second(p.second) {}
+
+		pair	&operator=(const pair &obj) : first(obj.first), second(obj.second) {
+			return *this;
+		}
+
+		return *this;
+	}
+
+	/*************************************************************
+	* MAKE_PAIR 
+	*************************************************************/
+	template <class T1, class T2>
+	pair<T1, T2> make_pair(const T1 t, const T2 u) { return pair<T1, T2>(t, u); }
+
+/*
+	// swap
+	template< class T1, class T2 >
+	void swap(pair<T1,T2> &x, pair<T1,T2> &y) {
+		pair<T1,T2>	tmp = x;
+		x = y; y = tmp;
+	}
+*/
+
+    template <class T1, class T2>
+    bool operator== (const pair<T1,T2> & l, const pair<T1,T2> & r)
+    { return (l.first == r.first && l.second == r.second); }
+
+    template <class T1, class T2>
+    bool operator<  (const pair<T1,T2> & l, const pair<T1,T2> & r)
+    { return (l.first < r.first || (l.first == r.first && l.second < r.second)); }
+
+    template <class T1, class T2>
+    bool operator!= (const pair<T1,T2> & l, const pair<T1,T2> & r)
+    { return (!(l == r)); }
+
+    template <class T1, class T2>
+    bool operator<= (const pair<T1,T2> & l, const pair<T1,T2> & r)
+    { return (!(r < l)); }
+
+    template <class T1, class T2>
+    bool operator>  (const pair<T1,T2> & l, const pair<T1,T2> & r)
+    { return (r < l); }
+
+    template <class T1, class T2>
+    bool operator>= (const pair<T1,T2> & l, const pair<T1,T2> & r)
+    { return (!(l < r)); }
+}
+
+#endif
