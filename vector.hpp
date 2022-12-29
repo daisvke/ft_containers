fa95d003d2cf78bef4a065be3f5481b921630028
dtanigaw <dtanigaw@student.42.fr>
2022-12-29 02:00:19 +0100

    Update vector

--- vector.hpp
+++ vector.hpp
@@ -38,18 +38,18 @@ namespace ft
 		/*************************************************************
 		 * Types
 		 *************************************************************/
-		typedef Alloc allocator_type;
-		typedef typename allocator_type::reference reference;
-		typedef typename allocator_type::const_reference const_reference;
-		typedef T value_type;
-		typedef ft::random_access_iterator<false, value_type> iterator;
-		typedef ft::random_access_iterator<true, value_type> const_iterator;
-		typedef std::size_t size_type;
-		typedef std::ptrdiff_t difference_type;
-		typedef typename allocator_type::pointer pointer;
-		typedef typename allocator_type::const_pointer const_pointer;
-		typedef ft::reverse_iterator<iterator> reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+		typedef Alloc											allocator_type;
+		typedef typename allocator_type::reference 				reference;
+		typedef typename allocator_type::const_reference		const_reference;
+		typedef T 												value_type;
+		typedef ft::random_access_iterator<false, value_type>	iterator;
+		typedef ft::random_access_iterator<true, value_type>	const_iterator;
+		typedef std::size_t										size_type;
+		typedef std::ptrdiff_t									difference_type;
+		typedef typename allocator_type::pointer 				pointer;
+		typedef typename allocator_type::const_pointer 			const_pointer;
+		typedef ft::reverse_iterator<iterator> 					reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;
 
 		/*************************************************************
 		 * Construct/Copy/Destroy
